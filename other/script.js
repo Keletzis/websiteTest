2417087e825ba29b7ed4a51993c7d206eea99b21
Kiosoglou Kaloudis Symeon <skaloudis@york.citycollege.eu>
2023-01-20 01:45:59 +0200

    Add simos

---
+++ other/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Get the top bar, menu button, and side menu elements
+var menuButton = document.querySelector('#menu-button');
+var sideMenu = document.querySelector('.side-menu');
+var darkModeToggle = document.querySelector('#dark-mode-toggle');
+var darkMode = false;
+
+// When the user clicks the menu button, toggle the "visible" class on the side menu
+menuButton.onclick = function () {
+    sideMenu.classList.toggle('visible');
+};
+darkModeToggle.onclick = function () {
+    document.body.classList.toggle('dark-mode');
+    darkMode = !darkMode;
+    if (darkMode) {
+        document.getElementById("imageToggle").src = "../images/darkmodeimage.jpg";
+    } else {
+        document.getElementById("imageToggle").src = "../images/sun.png";
+    }
+};
